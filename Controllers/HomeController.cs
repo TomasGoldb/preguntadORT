2ff8b5f4bbddfe6b8a958e8e04d79dc78aaa9211
Tomi <48588022@est.ort.edu.ar>
2024-09-24 22:51:44 -0300

    arreglo corona

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -264,7 +264,7 @@ public class HomeController : Controller
         ViewBag.PersonajesFoto=listaFotos;
         string [] listaNombres = {"arte","ciencia","deportes","entretenimiento","geografia","historia"};
         ViewBag.PersonajesNombres=listaNombres;
-        ViewBag.PersonajesConseguidos=Juego.ObtenerCantidadParaCorona(Sesion.partidaActual.IdPartida).Split("/");;
+        ViewBag.PersonajesConseguidos=Juego.ObtenerPersonajesConseguidos(Sesion.jugadorActual.IdJugador).Split("/");;
         return View("Corona");
     }
     public IActionResult PostCorona(string opcion){
